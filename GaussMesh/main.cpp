b1f6785eb6da18d6b7c1908065745a554dd84722
andikan <ejic045440@gmail.com>
2013-08-02 04:47:53 +0800

    Initial Commit

---
+++ GaussMesh/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  GaussMesh
+//
+//  Created by  Andikan on 13/8/2.
+//  Copyright (c) 2013年 Andikan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
